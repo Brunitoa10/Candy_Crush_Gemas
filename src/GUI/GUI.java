ce0dad2d74b037e99990a5063d0a5030aa7318c8
Bruno Parisi <bnoprs00@gmail.com>
2023-09-22 17:50:28 -0300

    Agregue extendido y cambie el main de gui a logica

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -1,6 +1,5 @@
 package GUI;
 
-import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -10,22 +9,7 @@ public class GUI extends JFrame {
 
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					GUI frame = new GUI();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
+	
 	/**
 	 * Create the frame.
 	 */
