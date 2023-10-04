af9a68d687c2096d705e76e300be691b015e247e
IgnacioCordiglia <ignaciocordiglia.contactos@gmail.com>
2023-10-04 17:51:46 -0300

    central de animaciones agregado

---
+++ src/Threads/Animador.java
@@ -0,0 +1,22 @@
+package Threads;
+
+import GUI.Celda;
+
+/**
+ * Define las operaciones esperables por sobre un elemento Animador.
+ * Un animador podrá llevar adelante la animación de movimiento o de cambio de estado, de una entidad.
+ * @author FJoaquin (federico.joaquin@cs.uns.edu.ar)
+ *
+ */
+public interface Animador {
+	/**
+	 * Obtiene la celda que se animará.
+	 * @return Retorna la celda obtenida.
+	 */
+	public Celda get_celda_asociada();
+	
+	/**
+	 * Inicia el comportamiento asociado con la animación.
+	 */
+	public void comenzar_animacion();
+}
