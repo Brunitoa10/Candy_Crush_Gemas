71168f8805c0c8b1514c98080fc8e536dac3d8ec
Bruno Parisi <bnoprs00@gmail.com>
2023-10-05 15:32:39 -0300

    Actualiacion distribucion gemas tablero

--- src/Nivel/GeneradorNivel.java
+++ src/Nivel/GeneradorNivel.java
@@ -5,51 +5,54 @@ import Entidades.GemaNormal;
 
 import java.util.Scanner;
 
+import Entidades.GemaEnvuelta;
+import Entidades.GemaRayada;
+import Entidades.Roca;
+import Logica.Color;
 import Tablero.Tablero;
 import java.io.File;
-
 public class GeneradorNivel {
-	 public static Nivel cargar_nivel_y_tablero(Tablero t, int nivel) {
-	        try {
-	            Scanner input = new Scanner(new File("Niveles/Nivel" + nivel + ".txt"));
+	public static Nivel cargar_nivel_y_tablero(Tablero t, int nivel) {
+        try {
+            Scanner input = new Scanner(new File("Niveles/Nivel" + nivel + ".txt"));
 
-	         // Leer tamaño del tablero
-	            int filas = Integer.parseInt(input.nextLine().trim());
-	            int columnas = filas;
+         // Leer tamaño del tablero
+            int filas = Integer.parseInt(input.nextLine().trim());
+            int columnas = filas;
 
-	            // Leer posición inicial
-	            String[] posicionInicial = input.nextLine().split(",");
-	            int posX = Integer.parseInt(posicionInicial[0].trim());
-	            int posY = Integer.parseInt(posicionInicial[1].trim());
-	         
-	            // Leer terminador
-	            String terminador = input.nextLine();
-	            if (!terminador.equals("f")) {
-	                throw new Exception("Formato de archivo inválido. Se esperaba un terminador 'f'.");
-	            }
-	            t.resetearTablero(filas, columnas);
-	            
-	            // Leer y establecer gemas
-	            for (int i = 0; i < filas; i++) {
-	                String[] valores = input.nextLine().split(" ");
-	                for (int j = 0; j < columnas; j++) {
-	                	t.agregarEntidad(new GemaNormal(i, j, Integer.parseInt(valores[j].trim())));
-	                }
-	            }
-	            input.close();
-	          System.out.println("Fila :: "+filas);
-	          System.out.println("Col :: "+columnas);
-	          System.out.println("PosX :: "+posX);
-	          System.out.println("posY :: "+posY);
-	       
+            // Leer posición inicial
+            String[] posicionInicial = input.nextLine().split(",");
+            int posX = Integer.parseInt(posicionInicial[0].trim());
+            int posY = Integer.parseInt(posicionInicial[1].trim());
+         
+            // Leer terminador
+            String terminador = input.nextLine();
+            if (!terminador.equals("f")) {
+                throw new Exception("Formato de archivo inválido. Se esperaba un terminador 'f'.");
+            }
+            t.resetearTablero(filas, columnas);
+            
+            // Leer y establecer gemas
+            for (int i = 0; i < filas; i++) {
+                String[] valores = input.nextLine().split(" ");
+                for (int j = 0; j < columnas; j++) {
+                	t.agregarEntidad(new GemaNormal(i, j, Integer.parseInt(valores[j].trim())));
+                }
+            }
+            input.close();
+          System.out.println("Fila :: "+filas);
+          System.out.println("Col :: "+columnas);
+          System.out.println("PosX :: "+posX);
+          System.out.println("posY :: "+posY);
+       
 
-	         
-	           return new Nivel(posX, posY);
-	        } catch (Exception ex) {
-	          System.out.println("GenerarNivelDefinitivo :: ME ROMPI");
-	          ex.getStackTrace();
-	            return null;
-	        }
-			
-	    }	    
+         
+           return new Nivel(posX, posY);
+        } catch (Exception ex) {
+          System.out.println("GenerarNivelDefinitivo :: ME ROMPI");
+          ex.getStackTrace();
+            return null;
+        }
+		
+    }	    
 }
--- src/Nivel/NivelPiolita.java
+++ src/Nivel/NivelPiolita.java
@@ -4,24 +4,16 @@ import Tablero.Tablero;
 
 public class NivelPiolita {
 	private Tablero tablero;
+	private String objetivo;
 	private int movimientosDisponibles;
 	private int tiempoDisponible;
-	private String objetivo;
-	//Atributos
 	protected int fila_inicial_jugador;
 	protected int columna_inicial_jugador;
-	
-	
-	
-	//Metodos
-	public int getFilaInicialJugador() {
-		return fila_inicial_jugador;
-	}
-	
+
 	public int getColumnaInicialJugador() {
 		return columna_inicial_jugador;
 	}
-	public NivelPiolita(Tablero tablero, int movimientosDisponibles, int tiempoDisponible, String objetivo,int f, int c) {
+	public NivelPiolita(int f, int c,Tablero tablero, int movimientosDisponibles, int tiempoDisponible, String objetivo) {
 		this.tablero = tablero;
 		this.movimientosDisponibles = movimientosDisponibles;
 		this.tiempoDisponible = tiempoDisponible;
@@ -44,6 +36,11 @@ public class NivelPiolita {
 
 	public String getObjetivo() {
 		return objetivo;
+	}
+
+
+	public int getFilaInicialJugador() {
+		return fila_inicial_jugador;
 	}
 
 	// Otros métodos según necesidad
