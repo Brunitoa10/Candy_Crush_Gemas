4bdf366bba3c2d4c6c61221955c312df761025dd
Bruno Parisi <bnoprs00@gmail.com>
2023-10-06 17:10:52 -0300

    Avances logicos generadorNivel, nivel y objetivos (posible version final)

--- src/Nivel/GeneradorNivel.java
+++ src/Nivel/GeneradorNivel.java
@@ -9,61 +9,72 @@ import Tablero.Tablero;
 import java.io.File;
 public class GeneradorNivel {
 	public static Nivel cargar_nivel_y_tablero(Tablero t, int nivel) {
-        try {
-        	Nivel miNivel = new Nivel(0,0);
-        	
-        	Scanner input = new Scanner(new File("Niveles/Nivel" + nivel + ".txt"));
-            
-         // Leer tamaño del tablero
-            int filas = Integer.parseInt(input.nextLine().trim());
-            int columnas = filas;
+		Nivel miNivel = new Nivel(0,0);
+		try {
+			Scanner input = new Scanner(new File("Niveles/Nivel" + nivel + ".txt"));
 
-            // Leer posición inicial
-            String[] posicionInicial = input.nextLine().split(",");
-        
-          miNivel.setPosicionJugador(Integer.parseInt(posicionInicial[0].trim()), Integer.parseInt(posicionInicial[1].trim()));
-         // Leer terminador
-            String terminador = input.nextLine();
-            if (!terminador.equals("t")) {
-                throw new Exception("Formato de archivo inválido. Se esperaba un terminador 't'.");
-            }
+			// Leer tamaño del tablero
+			int filas = Integer.parseInt(input.nextLine().trim());
+			int columnas = filas;
 
-            // Leer tipo y cantidad de gemas a destruir
-            
- 
-         // Leer objetivos hasta encontrar la marca 'f'
-            while (input.hasNextLine()) {
-                String line = input.nextLine();
-                if (line.equals("f")) {
-                    break;
-                }
-                String[] gemaData = line.split(",");
-                miNivel.agregarObjetivo(Integer.parseInt(gemaData[0].trim()), Integer.parseInt(gemaData[1].trim()));
-            }
-            
-            
-            t.resetearTablero(filas, columnas);
-            
-            // Leer y establecer gemas
-            for (int i = 0; i < filas; i++) {
-                String[] valores = input.nextLine().split(" ");
-                for (int j = 0; j < columnas; j++) {
-                	t.agregarEntidad(new GemaNormal(i, j, Integer.parseInt(valores[j].trim())));
-                }
-            }
-            input.close();
-          System.out.println("Fila :: "+filas);
-          System.out.println("Col :: "+columnas);
-          System.out.println("PosX :: "+miNivel.getFilaInicialJugador());
-          System.out.println("posY :: "+miNivel.getColumnaInicialJugador());
-      
-          
-           return miNivel;
-        } catch (Exception ex) {
-          System.out.println("GenerarNivelDefinitivo :: ME ROMPI");
-          ex.getStackTrace();
-            return null;
-        }
-		
-    }	    
+			// Leer posición inicial
+			String[] posicionInicial = input.nextLine().split(",");
+
+			miNivel.setPosicionJugador(Integer.parseInt(posicionInicial[0].trim()), Integer.parseInt(posicionInicial[1].trim()));
+			
+			//Leo cantidad de movimientos del nivel
+			miNivel.setMovimientos(Integer.parseInt(input.nextLine().trim()));
+			
+			//Leo tiempo del nivel
+			miNivel.setTiempo(Integer.parseInt(input.nextLine().trim()));
+
+			// Leer terminador
+			String terminador = input.nextLine();
+			if (!terminador.equals("t")) {
+				throw new Exception("Formato de archivo inválido. Se esperaba un terminador 't'.");
+			}
+
+			// Leer tipo y cantidad de gemas a destruir
+
+			//Mejorar
+			
+			// Leer objetivos hasta encontrar la marca 'f'
+			while (input.hasNextLine()) {
+				String line = input.nextLine();
+				if (line.equals("f")) {
+					break;
+				}
+				String[] gemaData = line.split(",");
+				miNivel.agregarObjetivo(Integer.parseInt(gemaData[0].trim()), Integer.parseInt(gemaData[1].trim()));
+			}
+
+
+			t.resetearTablero(filas, columnas);
+
+			// Leer y establecer gemas
+			for (int i = 0; i < filas; i++) {
+				String[] valores = input.nextLine().split(" ");
+				for (int j = 0; j < columnas; j++) {
+					t.agregarEntidad(new GemaNormal(i, j, Integer.parseInt(valores[j].trim())));
+				}
+			}
+			input.close();
+			System.out.println("------------------------------------------------");
+			System.out.println("Fila :: "+filas);
+			System.out.println("Col :: "+columnas);
+			System.out.println("PosX :: "+miNivel.getFilaInicialJugador());
+			System.out.println("posY :: "+miNivel.getColumnaInicialJugador());
+			System.out.println("Movimientos :: "+miNivel.getMovimientos());
+			System.out.println("Tiempo :: "+miNivel.getTiempo());
+			for(int i = 0; i < miNivel.sizeObjetivos(); i++) {
+				System.out.println("CantGemas :: "+miNivel.getObjetivo(i).getCantGemas()+" tipoGema :: "+miNivel.getObjetivo(i).getTipoGema());
+			}
+			System.out.println("------------------------------------------------");
+			
+		} catch (Exception ex) {
+			System.out.println("GenerarNivelDefinitivo :: ME ROMPI");
+			ex.getStackTrace();
+		}
+		return miNivel;
+	}	    
 }
--- src/Nivel/Nivel.java
+++ src/Nivel/Nivel.java
@@ -5,49 +5,66 @@ import java.util.ArrayList;
 
 public class Nivel {
 	//Atributos
-    protected int movimientos;
-    protected ArrayList<Objetivos> objetivo; // ArrayList para almacenar los objetivos
+	protected int movimientos;
+	protected ArrayList<Objetivos> objetivo; // ArrayList para almacenar los objetivos
 	protected int fila_inicial_jugador;
 	protected int columna_inicial_jugador;
-	
+	protected int tiempo;
+
 	//Constructor
-    public Nivel(int posX, int posY) {
-    	fila_inicial_jugador = posX;
-    	columna_inicial_jugador = posY;
-        this.movimientos = 15; 
-        this.objetivo = new ArrayList<>(); // Inicializamos la lista de objetivos
-    }
-	
+	public Nivel(int posX, int posY) {
+		fila_inicial_jugador = posX;
+		columna_inicial_jugador = posY;
+		//movimientos = tiempo = 0; 
+		this.objetivo = new ArrayList<>(); // Inicializamos la lista de objetivos
+	}
+
 	//Metodos
 	public int getFilaInicialJugador() {
 		return fila_inicial_jugador;
 	}
-	
+
 	public int getColumnaInicialJugador() {
 		return columna_inicial_jugador;
 	}
+
+	// Método para establecer los movimientos
+	public void setMovimientos(int movimientos) {
+		this.movimientos = movimientos;
+	}
+
+	// Método para establecer la posición del jugador
+	public void setPosicionJugador(int posX, int posY) {
+		fila_inicial_jugador = posX;
+		columna_inicial_jugador = posY;
+	}
+
+	public int getMovimientos() {
+		return movimientos;
+	}
+
+	public int getTiempo() {
+		return tiempo;
+	}
 	
-    // Método para establecer los movimientos
-    public void setMovimientos(int movimientos) {
-        this.movimientos = movimientos;
-    }
+	// Método para agregar un objetivo
+	public void agregarObjetivo(int cantGemas, int tipoGema) {
+		objetivo.add(new Objetivos(cantGemas, tipoGema));
+	}
 
-    // Método para establecer la posición del jugador
-    public void setPosicionJugador(int posX, int posY) {
-    	fila_inicial_jugador = posX;
-    	columna_inicial_jugador = posY;
+	public int sizeObjetivos() {
+		return objetivo.size();
+	}
+	public Objetivos getObjetivo(int indice) {
+		return objetivo.get(indice);
     }
+	public void setTiempo(int tiempo) {
+		this.tiempo = tiempo;
+	}
 
- // Método para agregar un objetivo
-    public void agregarObjetivo(int cantGemas, int tipoGema) {
-        objetivo.add(new Objetivos(cantGemas, tipoGema));
-    }
-
-    //MEJORAR ESTA IDEA
-    // Método para obtener los objetivos
-    public ArrayList<Objetivos> getObjetivos() {
-        return objetivo;
-    }
 	
-   
+
+
+
+
 }
