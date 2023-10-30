c9a6d3bda2796aa127ffb2e6dda78a9a22f41420
Santiago Reta Puccerelli <santiagoreta@yahoo.com>
2023-10-30 11:15:51 -0300

    correccion de algunos metodos de top score

---
+++ src/Score/FullListException.java
@@ -0,0 +1,10 @@
+package Score;
+
+public class FullListException extends Exception{
+
+    public FullListException(String mensaje)
+    {
+        super(mensaje);
+    }
+
+}
--- src/Score/TopScore.java
+++ src/Score/TopScore.java
@@ -7,14 +7,19 @@ import java.util.ArrayList;
 public class TopScore
  {
     private List<Jugador> listadeJugadores;
+    private static final int MAX_ELEMENTOS = 5;
 
     public TopScore() {
         listadeJugadores = new ArrayList<>();
     }
 
-    public void agregarJugador(String cadena, int numero) {
+    public void agregarJugador(String cadena, int numero) throws FullListException {
+        if(listadeJugadores.size()!=MAX_ELEMENTOS)
+        {
         Jugador ganador = new Jugador(cadena, numero);
         listadeJugadores.add(ganador);
+        }
+        else throw new FullListException ("Ya hay 5 jugadores");
     }
 
     public List<Jugador> obtenerListadeJugadores()
@@ -38,4 +43,6 @@ public class TopScore
             e.printStackTrace();
         }
     }
+
+    //SIEMPRE CARGEN ANTES DE AGREGAR Y GUARDEN DESPUES 
 }
