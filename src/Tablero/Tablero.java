cd70bd9eaf1473929963856e4cd02086a083cd99
Bruno Parisi <bnoprs00@gmail.com>
2023-11-23 23:29:56 -0300

    Avances tablero

--- src/Tablero/Tablero.java
+++ src/Tablero/Tablero.java
@@ -115,7 +115,7 @@ public class Tablero implements TableroJuego{
 		return nombreGema;
 	}
 
-	public void caida(int puntero,int columna ) {
+	/*public void caida(int puntero,int columna ) {
 		for (int i = puntero; i > 0; i--) {
 			if (entidades[i][columna].get_color() != 0) {
 				Entidad aux=entidades[i - 1][columna];
@@ -124,7 +124,7 @@ public class Tablero implements TableroJuego{
 			}
 		}
 		entidades[0][columna] = new GemaNormal(this,0,columna,new Color(new Random().nextInt(8)),true,skin);
-	}
+	}*/
 	
 	private void mover_jugador_auxiliar(int nf, int nc) {
 		if (en_rango(nf,nc)) {
@@ -197,35 +197,6 @@ public class Tablero implements TableroJuego{
 		entidades_asociadas.add(g.get_gema_interna());
 	}
 	
-/*	private boolean intercambiar_entidades_y_transicionar(int fila_destino, int columna_destino) {
-		int fila_origen = fJugador;
-		int columna_origen = cJugador;
-		Entidad entidad_origen, entidad_destino;
-		EfectosDeTransicion efecto_intercambio = null;
-		boolean movimientoValido = false;
-		
-		if (en_rango(fila_destino, columna_destino)) {	
-			entidad_origen = entidades[fila_origen][columna_origen];
-			entidad_destino = entidades[fila_destino][columna_destino];
-			
-			if (entidad_origen.es_posible_intercambiar(entidad_destino)) {
-				cambiar_posicion_jugador(fila_destino, columna_destino);
-				entidad_origen.intercambiar(entidad_destino);
-				efecto_intercambio = calcular_efectos_por_intercambio(entidad_origen, entidad_destino);
-				
-				if (efecto_intercambio.existen_entidades_a_detonar()) {
-					transicionar_proximo_estado(efecto_intercambio);
-				}else{
-					deshacer_intercambio(entidad_origen, entidad_destino, fila_origen, columna_origen);
-				}
-			}
-			System.out.println("TABLERO despues de DETONAR y reemplazar");
-			imprimirTablero();
-			movimientoValido = true;
-		}
-		return movimientoValido;
-	}*/
-	
 	private boolean intercambiar_entidades_y_transicionar(int filaDestino, int columnaDestino) {
 	    int filaOrigen = fJugador;
 	    int columnaOrigen = cJugador;
@@ -257,6 +228,7 @@ public class Tablero implements TableroJuego{
 	    EfectosDeTransicion efectoIntercambio = calcular_efectos_por_intercambio(entidadOrigen, entidadDestino);
 
 	    if (efectoIntercambio.existen_entidades_a_detonar()) {
+			System.out.println("Entre a existe_Entidades_a_detonar en realizarIntercambioYTransicion");
 	        transicionar_proximo_estado(efectoIntercambio);
 	    } else {
 	        deshacerIntercambio(entidadOrigen, entidadDestino, filaOrigen, columnaOrigen);
@@ -268,27 +240,10 @@ public class Tablero implements TableroJuego{
 	    entidadOrigen.intercambiar(entidadDestino);
 	}
 
-	/*protected EfectosDeTransicion calcular_efectos_por_intercambio(Entidad entidad_origen, Entidad entidad_destino){
-		 EfectosDeTransicion efecto_transicion = new EfectosDeTransicion();
-
-		// Verificar si se produce un match entre las entidades
-		Resultado resultado = miAdministradordeEstrategias.buscar_match(entidad_origen);
-
-		// Analizar el resultado para determinar los efectos
-		if (resultado != null && !resultado.get_Gemas_a_romper().isEmpty()) {
-			// Se produce un match, aplicar efectos correspondientes
-			for (Entidad entidad : resultado.get_Gemas_a_romper()) {
-				manejarMatch(efecto_transicion,entidad);
-			}
-		} else {
-			// No hay match, puedes agregar lógica adicional según tus requisitos
-		}
-		
-		return efecto_transicion;
-	}*/
 	protected EfectosDeTransicion calcular_efectos_por_intercambio(Entidad entidad_origen, Entidad entidad_destino){
 		EfectosDeTransicion efecto_transicion = new EfectosDeTransicion();
 		if (entidad_origen.se_produce_match_con(entidad_destino)) {
+			System.out.println("ENTRE en calcular_efectos_por_intercambio IF");
 			manejarMatch(efecto_transicion,entidad_origen);
 			manejarMatch(efecto_transicion,entidad_destino);
 		}else {
@@ -305,6 +260,11 @@ public class Tablero implements TableroJuego{
 		}
 		return efecto_transicion;
 	}
+	private void manejarMatch(EfectosDeTransicion efecto_transicion, Entidad entidad) {
+	    efecto_transicion.agregar_entidad_a_detonar_y_reemplazar(entidad);
+	    efecto_transicion.agregar_entidad_de_reemplazo(new GemaNormal(this, entidad.get_fila(), entidad.get_columna(), new Color(new Random().nextInt(6)*1), false, skin));
+	}
+
 	public boolean en_rango(int fila, int columna){
 		boolean en_rango_fila = (0 <= fila) && (fila < filas);
 		boolean en_rango_columna = (0 <= columna) && (columna < columnas);
@@ -335,16 +295,9 @@ public class Tablero implements TableroJuego{
 		}
 	}*/
 
-	
-	private void manejarMatch(EfectosDeTransicion efecto_transicion, Entidad entidad) {
-	    efecto_transicion.agregar_entidad_a_detonar_y_reemplazar(entidad);
-	    efecto_transicion.agregar_entidad_de_reemplazo(new GemaNormal(this, entidad.get_fila(), entidad.get_columna(), new Color(new Random().nextInt(6)*1), false, skin));
-	}
-
-	
 	protected void transicionar_proximo_estado(EfectosDeTransicion efecto_transicion) {
 		detonar(efecto_transicion.entidades_a_detonar());
-		agregar_entidades_nuevas(efecto_transicion.entidades_a_incorporar());
+		//agregar_entidades_nuevas(efecto_transicion.entidades_a_incorporar());
 		//aplicar_caida_y_reubicar(efecto_transicion.entidades_a_reemplazar());
 	}
 	
