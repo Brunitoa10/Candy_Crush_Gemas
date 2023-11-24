a6f69d36d870f8ff937a237afdffc8b6f49368e9
Bruno Parisi <bnoprs00@gmail.com>
2023-11-23 23:45:19 -0300

    Detonacion rayada y horizontal totalmente funcional

--- src/EstrategiaDetonaciones/EstrategiaDetonacionGemaRayadaHorizontal.java
+++ src/EstrategiaDetonaciones/EstrategiaDetonacionGemaRayadaHorizontal.java
@@ -6,39 +6,15 @@ import Tablero.Tablero;
 
 public class EstrategiaDetonacionGemaRayadaHorizontal implements EstategiaDetonacion{
 	@Override
+
 	public void detonar(Entidad entidad, Tablero tablero) {
-        int fila = entidad.get_fila();
-        int columna_adyacente = entidad.get_columna();
-        entidad.set_color(Color.TRANSPARENTE);
-        entidad.cargarImagenesRepresentativas(entidad.get_rutadeLaImagen());
-        entidad.get_EntidadGrafica().notificarse_detonacion();
-
-        detonarEntidadEnPosicion(tablero, fila, columna_adyacente - 1);
-        detonarEntidadEnPosicion(tablero, fila, columna_adyacente + 1);
-
-        // Realizar los intercambios y ajustes necesarios en la fila indicada
-        for (int columna = 0; columna < tablero.getColumna(); columna++) {
-            detonarEntidadEnPosicion(tablero, fila, columna);
-        }
-    }
-
-    private void detonarEntidadEnPosicion(Tablero tablero, int fila, int columna) {
-        if (esPosicionValida(tablero, fila, columna) && tablero.get_entidad(fila, columna).esAfectadaPorExplosionAdyacente()) {
-            tablero.get_entidad(fila, columna).detonar(tablero);
-        }
-    }
-
-    private boolean esPosicionValida(Tablero tablero, int fila, int columna) {
-        return fila >= 0 && fila < tablero.getFila() && columna >= 0 && columna < tablero.getColumna();
-    }
-	/*public void detonar(Entidad entidad, Tablero tablero) {
 	    int fila = entidad.get_fila();
 		int columna_adyacente=entidad.get_columna();
 	    entidad.set_color(Color.TRANSPARENTE);
 	    entidad.cargarImagenesRepresentativas(entidad.get_rutadeLaImagen());
 	    entidad.get_EntidadGrafica().notificarse_detonacion();
 
-		if(tablero.get_entidad(fila, columna_adyacente-1).esAfectadaPorExplosionAdyacente())
+		/*if(tablero.get_entidad(fila, columna_adyacente-1).esAfectadaPorExplosionAdyacente())
 		{
 			tablero.get_entidad(fila, columna_adyacente-1).detonar(tablero);
 		}
@@ -46,8 +22,14 @@ public class EstrategiaDetonacionGemaRayadaHorizontal implements EstategiaDetona
 		if(tablero.get_entidad(fila, columna_adyacente+1).esAfectadaPorExplosionAdyacente())
 		{
 			tablero.get_entidad(fila, columna_adyacente+1).detonar(tablero);
+		}*/
+		if (esPosicionValida(tablero, fila, columna_adyacente - 1) && tablero.get_entidad(fila, columna_adyacente - 1).esAfectadaPorExplosionAdyacente()) {
+			tablero.get_entidad(fila, columna_adyacente - 1).detonar(tablero);
 		}
-
+		
+		if (esPosicionValida(tablero, fila, columna_adyacente + 1) && tablero.get_entidad(fila, columna_adyacente + 1).esAfectadaPorExplosionAdyacente()) {
+			tablero.get_entidad(fila, columna_adyacente + 1).detonar(tablero);
+		}
 	    // Realizar los intercambios y ajustes necesarios en la fila indicada
 	    for (int columna = 0; columna < tablero.getColumna(); columna++) {
 	    	Entidad entidadActual = tablero.get_entidad(fila, columna);
@@ -55,5 +37,8 @@ public class EstrategiaDetonacionGemaRayadaHorizontal implements EstategiaDetona
 			entidadActual.cargarImagenesRepresentativas(entidadActual.get_rutadeLaImagen());
 			entidadActual.get_EntidadGrafica().notificarse_detonacion();
 	    }
-	}*/
+	}
+	 private boolean esPosicionValida(Tablero tablero, int fila, int columna) {
+        return fila >= 0 && fila < tablero.getFila() && columna >= 0 && columna < tablero.getColumna();
+    }
 }
--- src/EstrategiaDetonaciones/EstrategiaDetonacionGemaRayadaVertical.java
+++ src/EstrategiaDetonaciones/EstrategiaDetonacionGemaRayadaVertical.java
@@ -8,38 +8,12 @@ public class EstrategiaDetonacionGemaRayadaVertical implements EstategiaDetonaci
 
 	public void detonar(Entidad entidad, Tablero tablero) {
 		int columna = entidad.get_columna();
-		int fila_adyacente = entidad.get_fila();
-		entidad.set_color(Color.TRANSPARENTE);
-		entidad.cargarImagenesRepresentativas(entidad.get_rutadeLaImagen());
-		entidad.get_EntidadGrafica().notificarse_detonacion();
-	
-		detonarEntidadEnPosicion(tablero, fila_adyacente - 1, columna);
-		detonarEntidadEnPosicion(tablero, fila_adyacente + 1, columna);
-	
-		// Realizar los intercambios y ajustes necesarios en la columna indicada
-		for (int fila = 0; fila < tablero.getFila(); fila++) {
-			detonarEntidadEnPosicion(tablero, fila, columna);
-		}
-	}
-	
-	private void detonarEntidadEnPosicion(Tablero tablero, int fila, int columna) {
-		if (esPosicionValida(tablero, fila, columna) && tablero.get_entidad(fila, columna).esAfectadaPorExplosionAdyacente()) {
-			tablero.get_entidad(fila, columna).detonar(tablero);
-		}
-	}
-	
-	private boolean esPosicionValida(Tablero tablero, int fila, int columna) {
-		return fila >= 0 && fila < tablero.getFila() && columna >= 0 && columna < tablero.getColumna();
-	}
-	
-	/*public void detonar(Entidad entidad, Tablero tablero) {
-		int columna = entidad.get_columna();
 		int fila_adycaente=entidad.get_fila();
 		entidad.set_color(Color.TRANSPARENTE);
 		entidad.cargarImagenesRepresentativas(entidad.get_rutadeLaImagen());
 		entidad.get_EntidadGrafica().notificarse_detonacion();
 
-		if(tablero.get_entidad(fila_adycaente-1, columna).esAfectadaPorExplosionAdyacente())
+		/*if(tablero.get_entidad(fila_adycaente-1, columna).esAfectadaPorExplosionAdyacente())
 		{
 			tablero.get_entidad(fila_adycaente-1, columna).detonar(tablero);
 		}
@@ -47,8 +21,14 @@ public class EstrategiaDetonacionGemaRayadaVertical implements EstategiaDetonaci
 		if(tablero.get_entidad(fila_adycaente+1, columna).esAfectadaPorExplosionAdyacente())
 		{
 			tablero.get_entidad(fila_adycaente+1, columna).detonar(tablero);
+		}*/
+		if (esPosicionValida(tablero, fila_adycaente - 1, columna) && tablero.get_entidad(fila_adycaente - 1, columna).esAfectadaPorExplosionAdyacente()) {
+			tablero.get_entidad(fila_adycaente - 1, columna).detonar(tablero);
 		}
-
+		
+		if (esPosicionValida(tablero, fila_adycaente + 1, columna) && tablero.get_entidad(fila_adycaente + 1, columna).esAfectadaPorExplosionAdyacente()) {
+			tablero.get_entidad(fila_adycaente + 1, columna).detonar(tablero);
+		}
 		// Realizar los intercambios y ajustes necesarios en la columna indicada
 		for (int fila = 0; fila < tablero.getFila(); fila++) {
 			Entidad entidadActual = tablero.get_entidad(fila, columna);
@@ -56,5 +36,9 @@ public class EstrategiaDetonacionGemaRayadaVertical implements EstategiaDetonaci
 			entidadActual.cargarImagenesRepresentativas(entidadActual.get_rutadeLaImagen());
 			entidadActual.get_EntidadGrafica().notificarse_detonacion();
 		}
-	}*/
+		
+	}
+	private boolean esPosicionValida(Tablero tablero, int fila, int columna) {
+        return fila >= 0 && fila < tablero.getFila() && columna >= 0 && columna < tablero.getColumna();
+    }
 }
