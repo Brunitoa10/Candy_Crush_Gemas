e565459d32252e70414558d23034105df515a7ca
Bruno Parisi <91378645+Brunitoa10@users.noreply.github.com>
2023-12-05 03:11:11 -0300

    Modificacion en Animaciones

--- src/Threads/AnimadorCambioFoco.java
+++ src/Threads/AnimadorCambioFoco.java
@@ -10,54 +10,97 @@ import GUI.MultiIcon;
 
 public class AnimadorCambioFoco implements Animador {
 
+	/*
+	 * protected CentralAnimaciones manager;
+	 * protected Celda celda_animada;
+	 * protected int prioridad;
+	 * protected String[] path_imagenes_estados;
+	 * 
+	 * public AnimadorCambioFoco(CentralAnimaciones manager, Celda celda) {
+	 * this.manager = manager;
+	 * this.celda_animada = celda;
+	 * this.prioridad = PrioridadAnimaciones.PRIORIDAD_FOCO;
+	 * path_imagenes_estados =
+	 * celda.get_entidad_logica().get_imagenes_representativas();
+	 * }
+	 * 
+	 * public Celda get_celda_asociada() {
+	 * return celda_animada;
+	 * }
+	 * 
+	 * public int get_prioridad() {
+	 * return prioridad;
+	 * }
+	 * 
+	 * public void comenzar_animacion() {
+	 * if(path_imagenes_estados[1] == null) {
+	 * fijar_imagen_escalada_para_celda(path_imagenes_estados[0]);
+	 * } else fijar_imagen_escalada_para_celda_con_hielo(path_imagenes_estados[1],
+	 * path_imagenes_estados[0]);
+	 * celda_animada.repaint();
+	 * manager.notificarse_finalizacion_animador(this);
+	 * }
+	 * 
+	 * protected void fijar_imagen_escalada_para_celda_con_hielo(String
+	 * path_imagen1, String path_imagen2) {
+	 * int size_label = celda_animada.get_size_label();
+	 * ImageIcon icono_imagen1 = new
+	 * ImageIcon(this.getClass().getResource(path_imagen1));
+	 * Image imagen_escalada1 =
+	 * icono_imagen1.getImage().getScaledInstance(size_label, size_label,
+	 * Image.SCALE_SMOOTH);
+	 * Icon icono_imagen_escalado1 = new ImageIcon(imagen_escalada1);
+	 * 
+	 * ImageIcon icono_imagen2 = new
+	 * ImageIcon(this.getClass().getResource(path_imagen2));
+	 * Image imagen_escalada2 =
+	 * icono_imagen2.getImage().getScaledInstance(size_label, size_label,
+	 * Image.SCALE_SMOOTH);
+	 * Icon icono_imagen_escalado2 = new ImageIcon(imagen_escalada2);
+	 * 
+	 * MultiIcon iconoConHielo = new MultiIcon(icono_imagen_escalado1,
+	 * icono_imagen_escalado2);
+	 * iconoConHielo.setIconSize(size_label, size_label);
+	 * celda_animada.setIcon(iconoConHielo);
+	 * }
+	 * 
+	 * protected void fijar_imagen_escalada_para_celda(String path_imagen) {
+	 * int size_label = celda_animada.get_size_label();
+	 * ImageIcon icono_imagen = new
+	 * ImageIcon(this.getClass().getResource(path_imagen));
+	 * Image imagen_escalada = icono_imagen.getImage().getScaledInstance(size_label,
+	 * size_label, Image.SCALE_SMOOTH);
+	 * Icon icono_imagen_escalado = new ImageIcon(imagen_escalada);
+	 * celda_animada.setIcon(icono_imagen_escalado);
+	 * }
+	 */
 	protected CentralAnimaciones manager;
 	protected Celda celda_animada;
 	protected int prioridad;
-	protected String[] path_imagenes_estados;
+	protected String path_imagen_estado;
 
 	public AnimadorCambioFoco(CentralAnimaciones manager, Celda celda) {
 		this.manager = manager;
 		this.celda_animada = celda;
 		this.prioridad = PrioridadAnimaciones.PRIORIDAD_FOCO;
-		path_imagenes_estados = celda.get_entidad_logica().get_imagenes_representativas();
+		path_imagen_estado = celda.get_entidad_logica().get_imagenes_representativas()[0];
 	}
-	
+
 	public Celda get_celda_asociada() {
 		return celda_animada;
 	}
-	
+
 	public int get_prioridad() {
 		return prioridad;
 	}
 
 	public void comenzar_animacion() {
-		if(path_imagenes_estados[1] == null) {
-			fijar_imagen_escalada_para_celda(path_imagenes_estados[0]);
-		} else fijar_imagen_escalada_para_celda_con_hielo(path_imagenes_estados[1], path_imagenes_estados[0]);
-		celda_animada.repaint();
+		int size_label = celda_animada.get_size_label();
+		ImageIcon icono_imagen = new ImageIcon(this.getClass().getResource(path_imagen_estado));
+		Image imagen_escalada = icono_imagen.getImage().getScaledInstance(size_label, size_label,
+				Image.SCALE_REPLICATE);
+		Icon icono_escalado = new ImageIcon(imagen_escalada);
+		celda_animada.setIcon(icono_escalado);
 		manager.notificarse_finalizacion_animador(this);
-	}
-
-	protected void fijar_imagen_escalada_para_celda_con_hielo(String path_imagen1, String path_imagen2) {
-		int size_label = celda_animada.get_size_label();
-		ImageIcon icono_imagen1 = new ImageIcon(this.getClass().getResource(path_imagen1));
-		Image imagen_escalada1 = icono_imagen1.getImage().getScaledInstance(size_label, size_label, Image.SCALE_SMOOTH);
-		Icon icono_imagen_escalado1 = new ImageIcon(imagen_escalada1);
-	
-		ImageIcon icono_imagen2 = new ImageIcon(this.getClass().getResource(path_imagen2));
-		Image imagen_escalada2 = icono_imagen2.getImage().getScaledInstance(size_label, size_label, Image.SCALE_SMOOTH);
-		Icon icono_imagen_escalado2 = new ImageIcon(imagen_escalada2);
-	
-		MultiIcon iconoConHielo = new MultiIcon(icono_imagen_escalado1, icono_imagen_escalado2);
-		iconoConHielo.setIconSize(size_label, size_label);
-		celda_animada.setIcon(iconoConHielo);
-	}
-
-	protected void fijar_imagen_escalada_para_celda(String path_imagen) {
-		int size_label = celda_animada.get_size_label();
-		ImageIcon icono_imagen = new ImageIcon(this.getClass().getResource(path_imagen));
-		Image imagen_escalada = icono_imagen.getImage().getScaledInstance(size_label, size_label, Image.SCALE_SMOOTH);
-		Icon icono_imagen_escalado = new ImageIcon(imagen_escalada);
-		celda_animada.setIcon(icono_imagen_escalado);
 	}
 }
--- src/Threads/AnimadorDetonacion.java
+++ src/Threads/AnimadorDetonacion.java
@@ -1,4 +1,5 @@
 package Threads;
+
 import java.awt.Image;
 
 import javax.swing.Icon;
@@ -8,12 +9,75 @@ import GUI.Celda;
 
 public class AnimadorDetonacion extends Thread implements Animador {
 
+	/*
+	 * protected CentralAnimaciones manager;
+	 * protected Celda celda_animada;
+	 * protected int delay;
+	 * protected int prioridad;
+	 * protected String path_imagen_estado;
+	 * 
+	 * public AnimadorDetonacion(CentralAnimaciones manager, Celda celda, int delay)
+	 * {
+	 * this.manager = manager;
+	 * this.celda_animada = celda;
+	 * this.delay = delay;
+	 * this.prioridad = PrioridadAnimaciones.PRIORIDAD_DETONACION;
+	 * path_imagen_estado =
+	 * celda.get_entidad_logica().get_imagenes_representativas()[0];
+	 * }
+	 * 
+	 * public Celda get_celda_asociada() {
+	 * return celda_animada;
+	 * }
+	 * 
+	 * public int get_prioridad() {
+	 * return prioridad;
+	 * }
+	 * 
+	 * public void comenzar_animacion() {
+	 * this.start();
+	 * }
+	 * 
+	 * 
+	 * public void run() {
+	 * runAnimation();
+	 * }
+	 * 
+	 * public void runAnimation() {
+	 * int size_label = celda_animada.get_size_label();
+	 * try {
+	 * ImageIcon imagen_icon = new
+	 * ImageIcon(this.getClass().getResource("/assets/gemas/detonado.gif"));
+	 * Image imagen_escalada = imagen_icon.getImage().getScaledInstance(size_label,
+	 * size_label, Image.SCALE_REPLICATE);
+	 * Icon icono_escalado = new ImageIcon(imagen_escalada);
+	 * celda_animada.setIcon(icono_escalado);
+	 * sleep(700);
+	 * }catch (InterruptedException e) {
+	 * e.printStackTrace();
+	 * }
+	 * 
+	 * celda_animada.revalidate();
+	 * celda_animada.repaint();
+	 * 
+	 * ImageIcon iconoVacio = new ImageIcon(this.getClass().getResource(
+	 * "/assets/gemas/original/gema_normal/0.png"));
+	 * Image imgEscalada = iconoVacio.getImage().getScaledInstance(size_label,
+	 * size_label, Image.SCALE_SMOOTH);
+	 * Icon iconoEscalado = new ImageIcon(imgEscalada);
+	 * celda_animada.setIcon(iconoEscalado);
+	 * 
+	 * celda_animada.revalidate();
+	 * celda_animada.repaint();
+	 * manager.notificarse_finalizacion_animador(this);
+	 * }
+	 */
 	protected CentralAnimaciones manager;
 	protected Celda celda_animada;
 	protected int delay;
 	protected int prioridad;
 	protected String path_imagen_estado;
-	
+
 	public AnimadorDetonacion(CentralAnimaciones manager, Celda celda, int delay) {
 		this.manager = manager;
 		this.celda_animada = celda;
@@ -21,11 +85,11 @@ public class AnimadorDetonacion extends Thread implements Animador {
 		this.prioridad = PrioridadAnimaciones.PRIORIDAD_DETONACION;
 		path_imagen_estado = celda.get_entidad_logica().get_imagenes_representativas()[0];
 	}
-	
+
 	public Celda get_celda_asociada() {
 		return celda_animada;
 	}
-	
+
 	public int get_prioridad() {
 		return prioridad;
 	}
@@ -34,34 +98,20 @@ public class AnimadorDetonacion extends Thread implements Animador {
 		this.start();
 	}
 
-
-    public void run() {
-        runAnimation();
-    }
-	
-	public void runAnimation() {
+	public void run() {
 		int size_label = celda_animada.get_size_label();
+		ImageIcon imagen_icon = new ImageIcon(this.getClass().getResource(path_imagen_estado));
+		Image imagen_escalada = imagen_icon.getImage().getScaledInstance(size_label, size_label, Image.SCALE_REPLICATE);
+		Icon icono_escalado = new ImageIcon(imagen_escalada);
+		celda_animada.setIcon(icono_escalado);
+
 		try {
-			ImageIcon imagen_icon = new ImageIcon(this.getClass().getResource("/assets/gemas/detonado.gif"));
-			Image imagen_escalada = imagen_icon.getImage().getScaledInstance(size_label, size_label, Image.SCALE_REPLICATE);
-			Icon icono_escalado = new ImageIcon(imagen_escalada);
-			celda_animada.setIcon(icono_escalado);
-			sleep(700);
-		}catch (InterruptedException e) {
+			sleep(delay);
+		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 
-		celda_animada.revalidate();
-		celda_animada.repaint();
-
-		ImageIcon iconoVacio = new ImageIcon(this.getClass().getResource("/assets/gemas/original/gema_normal/0.png"));
-		Image imgEscalada = iconoVacio.getImage().getScaledInstance(size_label, size_label, Image.SCALE_SMOOTH);
-		Icon iconoEscalado = new ImageIcon(imgEscalada);
-		celda_animada.setIcon(iconoEscalado);
-
-		celda_animada.revalidate();
-		celda_animada.repaint();
+		celda_animada.eliminar_de_ventana();
 		manager.notificarse_finalizacion_animador(this);
 	}
-
 }
